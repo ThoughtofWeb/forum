cd434b6c10af47e7b26714d2fb82130b8faf3659
Cynthia <caoxiangx@163.com>
2019-06-19 14:28:23 +0800

    update

--- src/common/pages/home/components/Topic.js
+++ src/common/pages/home/components/Topic.js
@@ -1,20 +1,20 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {TopicWrapper,TopicItem,TopicImg} from '../style';
+import {TopicImg, TopicItem, TopicWrapper} from '../style';
 
 class Topic extends Component {
     render() {
         return (
             <TopicWrapper>
                 {
-                    this.props.list.map((item)=>{
-                        return(
+                    this.props.list.map((item) =>
+                        (
                             <TopicItem key={item.get('id')}>
                                 <TopicImg className="topic-pic" imgUrl={item.get('imgUrl')}/>
                                 {item.get('title')}
                             </TopicItem>
                         )
-                    })
+                    )
                 }
 
 
@@ -23,7 +23,7 @@ class Topic extends Component {
     }
 }
 
-const mapState=(state)=>({
-    list:state.home.get('topicList')
+const mapState = (state) => ({
+    list: state.home.get('topicList')
 })
-export default connect(mapState,null)(Topic);
+export default connect(mapState, null)(Topic);
